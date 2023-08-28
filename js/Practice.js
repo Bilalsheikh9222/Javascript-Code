72ae58381efa85af15a678240f92a586ed78cfeb
Bilalsheikh9222 <bilalsheikh9222@gmail.com>
2023-08-28 16:49:37 +0500

    update practice file

--- js/Practice.js
+++ js/Practice.js
@@ -1,18 +1,7 @@
-document.write("<h1> You're Result </h1>");
+document.wriite("<h1>Practice</h1>");
 
-var per = prompt("you're per is: ");
-if(per >= 90 && per <= 100){
-    document.write("merit");
-}else if(per >= 80 && per < 90){
-    document.write("You're first");
-}else if(per >= 60 && per < 80){
-    document.write("You're 2nd");
-}else if(per >= 40 && per < 60){
-    document.write("You're passed");
-}else if(per >= 20 && per < 40){
-    document.write("Promote");
-}else if(per >= 5 && per < 20){
-    document.write("Fail");
-}else{
-    document.write("Please enter the valid number...");
+function funN(){
+    document.write("This is a function");
 }
+
+funN();
