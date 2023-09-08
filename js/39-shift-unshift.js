a6f3d96a77622060fca806343eff034bde106f8e
Bilalsheikh9222 <bilalsheikh9222@gmail.com>
2023-09-08 16:51:58 +0500

    update file

--- js/39-shift-unshift.js
+++ js/39-shift-unshift.js
@@ -1,11 +1,11 @@
 document.write("<h1> 39 - Shift - Unshift </h1>");
 
-var a = ["this","is","shift","delete","array","index","one"];
+var a = ["this","is","shift","delete","array","index","zero"];
 
 a.shift();
 document.write(a);
 
 document.write("<br/><br/>");
 
-a.unshift("<b>unshift</b> add value on index one");
+a.unshift("<b>unshift</b> add value on index zero");
 document.write(a);
--- js/Practice.js
+++
@@ -1,7 +0,0 @@
-document.wriite("<h1>Practice</h1>");
-
-function funN(){
-    document.write("This is a function");
-}
-
-funN();
